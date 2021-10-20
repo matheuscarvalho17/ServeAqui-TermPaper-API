ccb27fab378063d7bc624fd2496fde9e3b6c76f9
MatheusCarvalho <matheusinholopescarvalho@gmail.com>
2021-10-20 15:08:15 -0300

    Configurações sequelize

--- app.ts
+++ app.ts
@@ -1,8 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 
 const port = 3000;
 const app = express();
+import bodyParser from 'body-parser';
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, () => {
-  console.log(`Servidor está rodando na porta ${port}.`);
+  console.log(`Servidor backend está rodando.`);
+});
+
+app.get('/', (req, res) => {
+  res.send(`Rodando GET na porta ${port}.`);
 });
