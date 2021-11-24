b875cdb4d098867a0e8654de1d00113933273e26
MatheusCarvalho <matheusinholopescarvalho@gmail.com>
2021-11-24 16:16:38 -0300

    Atualização e criação de novas Querys

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ServeAqui-API
-  Trabalho de conclusão de Curso de Engenharia da Computação, por Matheus Carvalho.
+  Trabalho de conclusão de Curso em Bacharel de Engenharia da Computação, por Matheus Carvalho.
+  Este projeto se refere ao back-end do trabalho, onde se faz o uso de bibliotecas como: Cors, Express, MySQL2 e Sequelize para criação da API.
+
--- app.ts
+++ app.ts
@@ -3,33 +3,52 @@ import models from './models';
 import express from 'express';
 import Sequelize from 'sequelize';
 
+//All constants declaration
 const port = 3000;
 const app = express();
 const Op = Sequelize.Op;
-
 const user = models.User;
+const order = models.Order;
 const table = models.Table;
+const request = models.Request;
 const product = models.Product;
+const arrayTables = ['id', 'name', 'status', 'callWaiter'];
+const arrayProduct = [
+  'id',
+  'type',
+  'price',
+  'title',
+  'amount',
+  'editable',
+  'image_url',
+  'description',
+  'ingredients',
+];
 
+//All required extension declarations
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//
+//Notice
+//
+//Operation notice
 app.listen(port, () => {
   console.log(`Servidor backend está rodando.`);
-});
-app.get('/', (req, res) => {
-  res.send('Servidor rodando!');
 });
 
 //
 //Users
-
+//
 //Post to login
 app.post('/ws/users/login', async (req, res) => {
   const response = await user.findOne({
-    where: { credential: req.body.credential, password: req.body.password },
     attributes: ['id'],
+    where: {
+      password: req.body.password,
+      credential: req.body.credential,
+    },
   });
   if (response === null) {
     res.send({ warm: 'Credencial inválida ou senha incorreta!' });
@@ -40,16 +59,15 @@ app.post('/ws/users/login', async (req, res) => {
 
 //
 //Products
-
+//
 //Get all products and products by type
 app.get('/ws/products', async (req, res) => {
-  let typeRequest = req.query;
-
   const response = await product.findAll({
+    attributes: arrayProduct,
     where:
-      typeRequest.type != null
+      req.query.type != null
         ? {
-            type: typeRequest.type,
+            type: req.query.type,
           }
         : {},
   });
@@ -58,14 +76,13 @@ app.get('/ws/products', async (req, res) => {
 
 //Get products by name
 app.get('/ws/products/name', async (req, res) => {
-  let titleRequest = req.query;
-
   const response = await product.findAll({
+    attributes: arrayProduct,
     where:
-      titleRequest.title != null
+      req.query.title != null
         ? {
             title: {
-              [Op.like]: '%' + titleRequest.title + '%',
+              [Op.like]: '%' + req.query.title + '%',
             },
           }
         : {},
@@ -75,13 +92,12 @@ app.get('/ws/products/name', async (req, res) => {
 
 //Get product by id
 app.get('/ws/product', async (req, res) => {
-  let idRequest = req.query;
-
   const response = await product.findOne({
+    attributes: arrayProduct,
     where:
-      idRequest.id != null
+      req.query.id != null
         ? {
-            id: idRequest.id,
+            id: req.query.id,
           }
         : {},
   });
@@ -90,32 +106,77 @@ app.get('/ws/product', async (req, res) => {
 
 //
 //Tables
-
-//Get all tables by id
+//
+//Get all tables
 app.get('/ws/tables', async (req, res) => {
   const response = await table.findAll({
-    attributes: ['id', 'name', 'status', 'callWaiter'],
+    attributes: arrayTables,
   });
   res.send(response);
 });
 
-//
-//
-//
-//
-//
-//
-// app.get('/update', async (req, res) => {
-//   let update = await user
-//     .findByPk(2, { include: [{ all: true }] })
-//     .then((response) => {
-//       response.Trackings[0].local = 'Nova Cidade';
-//       response.Trackings[0].save();
-//     });
-// });
+//Get table by id
+app.get('/ws/table', async (req, res) => {
+  const response = await table.findOne({
+    attributes: arrayTables,
+    where:
+      req.query.id != null
+        ? {
+            id: req.query.id,
+          }
+        : {},
+  });
+  res.send(response);
+});
 
-// app.get('/delete', async (req, res) => {
-//   user.destroy({
-//     where: { id: 2 },
-//   });
-// });
+//Edit table status by id
+app.put('/ws/table/status', async (req, res) => {
+  await table.update(
+    { status: req.body.status },
+    {
+      where: {
+        id: req.body.idTable,
+      },
+    },
+  );
+  res.send({ warm: 'Solicitação realizada com sucesso!' });
+});
+
+//Call Waiter on table by id
+app.put('/ws/table/callwaiter', async (req, res) => {
+  await table.update(
+    { callWaiter: req.body.callWaiter },
+    {
+      where: {
+        id: req.body.idTable,
+      },
+    },
+  );
+  res.send({ warm: 'Solicitação realizada com sucesso!' });
+});
+
+//
+//Orders and Requests
+//
+//Create new order
+app.post('/ws/comercial/order/create', async (req, res) => {
+  await order.create({
+    status: req.body.status,
+    idTable: req.body.idTable,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+  res.send({ warm: 'Pedido criada com sucesso!' });
+});
+
+//Create new request
+app.post('/ws/comercial/request/create', async (req, res) => {
+  await request.create({
+    amount: req.body.amount,
+    idOrder: req.body.idOrder,
+    idProduct: req.body.idProduct,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+  res.send({ warm: 'Solicitação criada com sucesso!' });
+});
